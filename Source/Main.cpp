5cd0d5bfd29565f9035c1dc22f7d8def07607b98
soemono <53477278+soemono@users.noreply.github.com>
2019-09-22 13:24:30 +0900

    Ver1.1になりました
    フィールドが大きくなりました。
    ランダムにセルを削除する機能を追加しました。

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,42 +1,103 @@
-﻿
+
 # include <Siv3D.hpp> // OpenSiv3D v0.4.0
 
-void Main()
-{
-	//グリッドの高さ幅を変更
-	const int16 height = 200;
-	const int16 width = 300;
+//セルの生死を決めるルール用構造体
+struct automatonrule {
+	Array<bool> live = { 0,0,1,1,0,0,0,0,0 };
+	Array<bool> dead = { 0,0,0,1,0,0,0,0,0 };
+};
 
-	//表示倍率
-	int16 ntimes = 2;
-
+//初期化用関数,変数以外はなるべくここに入れよう
+void init(const int16 &width,const int16 &height,const int16 &ntimes) {
 	//ウィンドウの名前を変更
 	Window::SetTitle(U"lifegame");
 
 	//ウィンドウの初期サイズを適用
 	Window::Resize(Size(width * ntimes, height * ntimes));
 
+	//背景色を変更
+	Scene::SetBackground(ColorF(0.05, 0.05, 0.05));
+}
+
+//fieldの状態を見て作ったフィールドをnextfieldに渡します
+void fieldupdate(Grid<bool>& field, Grid<bool>& nextfield, automatonrule Rule, const int16& height, const int16& width, const int16& outofFrame) {
+	for (auto y : step(height + ((outofFrame - 1) * 2))) {
+		for (auto x : step(width + ((outofFrame - 1) * 2))) {
+			int8 count = 0;
+
+			//番兵を置いてあるためちょっとオフセットしてある
+			int32 ysum = y + 1;
+			int32 xsum = x + 1;
+
+			//周囲のセルをカウント
+			if (field[ysum - 1][xsum - 1]) ++count;
+			if (field[ysum - 1][xsum]) ++count;
+			if (field[ysum - 1][xsum + 1]) ++count;
+			if (field[ysum][xsum - 1]) ++count;
+			if (field[ysum][xsum + 1]) ++count;
+			if (field[ysum + 1][xsum - 1]) ++count;
+			if (field[ysum + 1][xsum]) ++count;
+			if (field[ysum + 1][xsum + 1]) ++count;
+
+			//配列でセルの生死をいじれるようになっている
+			nextfield[ysum][xsum] = (
+				field[ysum][xsum] && (
+					(count == 0 && Rule.live[0]) ||
+					(count == 1 && Rule.live[1]) ||
+					(count == 2 && Rule.live[2]) ||
+					(count == 3 && Rule.live[3]) ||
+					(count == 4 && Rule.live[4]) ||
+					(count == 5 && Rule.live[5]) ||
+					(count == 6 && Rule.live[6]) ||
+					(count == 7 && Rule.live[7]) ||
+					(count == 8 && Rule.live[8])
+				) || !field[ysum][xsum] && (
+					(count == 0 && Rule.dead[0]) ||
+					(count == 1 && Rule.dead[1]) ||
+					(count == 2 && Rule.dead[2]) ||
+					(count == 3 && Rule.dead[3]) ||
+					(count == 4 && Rule.dead[4]) ||
+					(count == 5 && Rule.dead[5]) ||
+					(count == 6 && Rule.dead[6]) ||
+					(count == 7 && Rule.dead[7]) ||
+					(count == 8 && Rule.dead[8])
+				)
+				);
+		}
+	}
+}
+
+void Main()
+{
+	//グリッドの高さ、幅を変更
+	constexpr int16 height = 250;
+	constexpr int16 width = 400;
+
+	//画面枠外領域の広さを設定
+	constexpr int16 outofFrame = 3;
+
+	//表示倍率
+	int16 ntimes = 2;
+
 	//フィールド、次回フィールド、色用フィールドなどを設定
-	Grid<bool> field(width + 6, height + 6, false);
-	Grid<bool> nextfield(width + 6, height + 6, false);
-	Grid<bool> beforefield(width + 6, height + 6, false);
+	Grid<bool> field(width + (outofFrame * 2), height + (outofFrame * 2), false);
+	Grid<bool> nextfield(width + (outofFrame * 2), height + (outofFrame * 2), false);
+	Grid<bool> beforefield(width + (outofFrame * 2), height + (outofFrame * 2), false);
 	Grid<double> colorfield(width, height, 0.0);
 
 	//ライフゲーム描画用画像、テクスチャ
 	Image image(width, height, ColorF(0,0,0,0));
 	DynamicTexture texture(image);
 
-	//グリッド描画用テクスチャ
+	//グリッド描画用画像、テクスチャ
 	Image gridimage(width * 4, height * 4, ColorF(0, 0, 0, 0));
-
 	DynamicTexture gridtexture(gridimage);
-
-	//接触判定用長方形、ウィンドウサイズに合わせる
 
 	//スイッチ用変数
 	bool toedit = false;
 	bool editblush = false;
-	bool fieldrandmize = false;
+	bool fieldrandfill = false;
+	bool fieldranddel = false;
 	bool showoption = false;
 	size_t xtimebutton = 1;
 	bool randomthing = true;
@@ -46,6 +107,7 @@ void Main()
 	bool windowresize = false;
 	bool drawgrid = false;
 	bool continuegrid = false;
+	//速度系変数
 	double colorspeed = 0.04;
 	double colorspeedadjust = 0.0;
 	double autostepspeed = 0.32;
@@ -56,108 +118,76 @@ void Main()
 
 	//描画更新用スイッチ
 	bool updated = false;
-
-	//色相用変数
-	int32 huenum = 0;
+	bool needdraw = true;
 
 	//フォント
 	Font fontsmall(14);
 	Font fontnormal(20);
 
-	//セルの生死を決めるルール用配列	
-	Array<bool> livenum = { 0,0,1,1,0,0,0,0,0 };
-	Array<bool> deadnum = { 0,0,0,1,0,0,0,0,0 };
-
 	//汎用ストップウォッチ
 	Stopwatch stopwatch(true);
 
+	//ルール定義構造体
+	automatonrule Rule;
+
+	//初期化用関数
+	init(width, height, ntimes);
+
 	//線形補間を無効にする、メインループ内にあると例外吐いて落ちたのでここに配置
+	//initに入れるとうまく反映されないのでここに配置しときます
 	ScopedRenderStates2D sampler(SamplerState::ClampNearest);
 
 	while (System::Update())
 	{
-		Scene::SetBackground(ColorF(0, 0, 0.1));
 
 		//ライフゲーム動作部分
 		if ((autostep || steptostep) && stopwatch.sF() >= (autostepspeed * autostepspeed * autostepspeed)) {
 			stopwatch.restart();
 
-			//フィールドランダム埋め
-			if (fieldrandmize) {
+			//フィールドランダム埋め、連打するとセルが増えていくようにしてある
+			if (fieldrandfill) {
 				for (auto y : step(height)) {
 					for (auto x : step(width)) {
 						if (RandomBool(0.2)) {
-							field[y + 3][x + 3] = true;
+							field[y + outofFrame][x + outofFrame] = true;
 						}
 					}
 				}
-				fieldrandmize = false;
+				fieldrandfill = false;
+			}
+
+			if (fieldranddel) {
+				for (auto y : step(height)) {
+					for (auto x : step(width)) {
+						if (RandomBool(0.2)) {
+							field[y + outofFrame][x + outofFrame] = false;
+						}
+					}
+				}
+				fieldranddel = false;
 			}
 
 			//ランダムなぼわぼわが下に出る
 			if (randomthing) {
 				for (auto y : step(3)) {
 					for (auto x : step(width)) {
-						field[height + 3 - y][x + 1] = RandomBool(0.5);
+						field[height + outofFrame - y][x + 1] = RandomBool(0.5);
 					}
 				}
 			}
 
 			//ライフゲーム部
-			for (auto y : step(height + 4)) {
-				for (auto x : step(width + 4)) {
-					int8 count = 0;
-
-					//番兵を置いてあるためちょっとオフセットしてある
-					int32 ysum = y + 1;
-					int32 xsum = x + 1;
-
-					//周囲のセルをカウント
-					if (field[ysum - 1][xsum - 1]) ++count;
-					if (field[ysum - 1][  xsum  ]) ++count;
-					if (field[ysum - 1][xsum + 1]) ++count;
-					if (field[  ysum  ][xsum - 1]) ++count;
-					if (field[  ysum  ][xsum + 1]) ++count;
-					if (field[ysum + 1][xsum - 1]) ++count;
-					if (field[ysum + 1][  xsum  ]) ++count;
-					if (field[ysum + 1][xsum + 1]) ++count;
-
-					//配列でセルの生死をいじれるようになっている
-					nextfield[ysum][xsum] = (
-						field[ysum][xsum] && (
-							(count == 0 && livenum[0]) ||
-							(count == 1 && livenum[1]) ||
-							(count == 2 && livenum[2]) ||
-							(count == 3 && livenum[3]) ||
-							(count == 4 && livenum[4]) ||
-							(count == 5 && livenum[5]) ||
-							(count == 6 && livenum[6]) ||
-							(count == 7 && livenum[7]) ||
-							(count == 8 && livenum[8])
-						) || !field[ysum][xsum] && (
-							(count == 0 && deadnum[0]) ||
-							(count == 1 && deadnum[1]) ||
-							(count == 2 && deadnum[2]) ||
-							(count == 3 && deadnum[3]) ||
-							(count == 4 && deadnum[4]) ||
-							(count == 5 && deadnum[5]) ||
-							(count == 6 && deadnum[6]) ||
-							(count == 7 && deadnum[7]) ||
-							(count == 8 && deadnum[8])
-						)
-					);
-				}
-			}
+			fieldupdate(field,nextfield,Rule,height,width,outofFrame);
 
 			//色フィールド設定部	
 			for (auto y : step(height)) {
 				for (auto x : step(width)) {
-					if (field[y + 3][x + 3] == true) {
+					if (field[y + outofFrame][x + outofFrame] == true) {
 						colorfield[y][x] = 1.0;
 					}
 					else {
 						if (colorfield[y][x] > 0) {
-							//colorfieldはスライダーによって変更できる変数colorspeedによって減少する
+							//colorfieldはスライダーによって変更できる変数colorspeedとcoloradjustによって減少する
 							double speed = colorspeed + colorspeedadjust;
 							colorfield[y][x] -= (speed * speed);
 						} else if (colorfield[y][x] != 0) {
@@ -170,8 +200,6 @@ void Main()
 			updated = true;
 		}
 
-	
-
 		//フィールド更新
 		if (autostep || steptostep) {
 			beforefield = field;
@@ -179,7 +207,7 @@ void Main()
 			steptostep = false;
 		}
 
-		//エディット処理(製作中)
+		//エディット処理
 
 		int32 cursorx = (Cursor::Pos().x / ntimes);
 		int32 cursory = (Cursor::Pos().y / ntimes);
@@ -187,28 +215,34 @@ void Main()
 		if (toedit) {
 			ClearPrint();
 			Print(U"Enterで抜ける");
-			bool outofrange = (cursorx < 0 || cursorx > width - 1 || cursory < 0 || cursory > height);
-			if (MouseL.down() && !outofrange) {
-				editblush = !field[cursory + 3][cursorx + 3];
-			}
+			//枠外に出たときの変数
+			
+			bool outofrange = (cursorx < 0 || cursorx >= width || cursory < 0 || cursory >= height);
 
-			if (MouseL.pressed() && !outofrange) {
-				if (editblush) {
-					image[cursory][cursorx] = ColorF(Palette::White);
+			if (!outofrange) {
+				if (MouseL.down()) {
+					editblush = !field[cursory + outofFrame][cursorx + outofFrame];
 				}
-				else {
-					if (colorfield[cursory][cursorx] > 0) {
-						image[cursory][cursorx] = ColorF(colorfield[cursory][cursorx] / 8, colorfield[cursory][cursorx] / 2, colorfield[cursory][cursorx]);
+
+				if (MouseL.pressed()) {
+					if (editblush) {
+						image[cursory][cursorx] = ColorF(Palette::White);
 					}
 					else {
-						image[cursory][cursorx] = ColorF(0,0,0,0);
+						if (colorfield[cursory][cursorx] > 0) {
+							image[cursory][cursorx] = HSV(213, 1, 1, colorfield[cursory][cursorx]);
+						}
+						else {
+							image[cursory][cursorx] = ColorF(0, 0, 0, 0);
+						}
+
+						if (beforefield[cursory + outofFrame][cursorx + outofFrame]) {
+							image[cursory][cursorx] = ColorF(Palette::Pink);
+						}
 					}
-					
-					if (beforefield[cursory + 3][cursorx + 3]) {
-						image[cursory][cursorx] = ColorF(Palette::Pink);
-					}
+					field[cursory + outofFrame][cursorx + outofFrame] = editblush;
+					needdraw = true;
 				}
-				field[cursory + 3][cursorx + 3] = editblush;
 			}
 		}
 
@@ -217,8 +251,8 @@ void Main()
 
 			for (auto y : step(height)) {
 				for (auto x : step(width)) {
-					int32 ysum = y + 3;
-					int32 xsum = x + 3;
+					int32 ysum = y + outofFrame;
+					int32 xsum = x + outofFrame;
 					if (beforefield[ysum][xsum] || field[ysum][xsum]) {
 						image[y][x] = field[ysum][xsum] ? Color(Palette::White) : Color(Palette::Pink);
 					}
@@ -231,17 +265,13 @@ void Main()
 				}
 			}
 			updated = false;
+			needdraw = true;
 		}
-		
-		//テクスチャをimageに書き換え
-		texture.fill(image);
-
-		//線形補間を無効にしてアンチエイリアスを消し、n倍で描画
-		texture.scaled(ntimes).draw();
 
 		//グリッド描画
 		if (drawgrid || windowresize) {
-			constexpr ColorF gridcolor(0.2, 0.2, 0.2, 0.5);
+			//グリッドを作成する
+			constexpr ColorF gridcolor(0.5, 0.5, 0.5, 0.3);
 
 			for (int32 y : step(height* ntimes)) {
 				if ((y % ntimes) == 0) {
@@ -255,7 +285,7 @@ void Main()
 							gridimage[y][x] = gridcolor;
 						}
 						else {
-							gridimage[y][x] = ColorF(0,0,0,0);
+							gridimage[y][x] = ColorF(0, 0, 0, 0);
 						}
 					}
 				}
@@ -264,9 +294,21 @@ void Main()
 			gridtexture.fill(gridimage);
 		}
 
-		if (continuegrid && ntimes >= 2) {
-			gridtexture.draw();
+		if (needdraw) {
+			//テクスチャをimageに書き換え
+			texture.fill(image);
+
+			//n倍で描画
+			texture.scaled(ntimes).draw();
+
+			//グリッドを描画する
+			if (continuegrid && ntimes >= 2) {
+				gridtexture.draw();
+			}
 		}
+
+		
+		
 
 		//描画したあとにウィンドウサイズの更新を行う
 		if (windowresize) {
@@ -333,7 +375,7 @@ void Main()
 					windowresize = true;
 				}
 
-				SimpleGUI::Slider(U"",autostepspeed, 0.01, 1, Vec2(10, 50),0,150);
+				SimpleGUI::Slider(U"",autostepspeed, 1, 0.01, Vec2(10, 50),0,150);
 				
 
 			} else if (!showoption && !toedit) {
@@ -342,7 +384,7 @@ void Main()
 				//------------------左端揃えボタン---------------------
 				
 				//枠の角丸長方形
-				RoundRect(5, 5, 255, 206, 5).draw();
+				RoundRect(5, 5, 265, 206, 5).draw();
 
 				//スタートストップボタン
 				constexpr int32 Playbuttonposx = 11;
@@ -369,14 +411,17 @@ void Main()
 
 				//リセットボタン
 				if (SimpleGUI::Button(U"全消去", Vec2(175, 10), 70)) {
-					for (auto y : step(height + 6)) {
-						for (auto x : step(width + 6)) {
+
+					//フィールドの初期化
+					for (auto y : step(height + (outofFrame * 2))) {
+						for (auto x : step(width + outofFrame * 2)) {
 							beforefield[y][x] = 0;
 							field[y][x] = 0;
 							nextfield[y][x] = 0;
 						}
 					}
 
+					//画像と色の初期化
 					for (auto y : step(height)) {
 						for (auto x : step(width)) {
 							image[y][x] = Color(Palette::Black);
@@ -386,23 +431,27 @@ void Main()
 				}
 
 				//フィールドをランダム初期値で埋める
-				if (SimpleGUI::Button(U"ランダムに埋める", Vec2(10, 50),190)) {
-					fieldrandmize = true;
+				if (SimpleGUI::Button(U"ランダム埋め", Vec2(10, 50),125)) {
+					fieldrandfill = true;
+				}
+
+				if (SimpleGUI::Button(U"ランダム消去", Vec2(140, 50), 125)) {
+					fieldranddel = true;
 				}
 
 				//世代が変わるスピードの変更
-				SimpleGUI::Slider(U"速度", autostepspeed, 0.01, 1, Vec2(10, 90),50,190);
+				SimpleGUI::Slider(U"速度", autostepspeed, 1,0.01, Vec2(10, 90),50,190);
 
 				//ランダムなやつ入り切りボタン
-				SimpleGUI::CheckBox(randomthing, U"ランダム入力", Vec2(8, 130));
+				SimpleGUI::CheckBox(randomthing, U"ランダム入力", Vec2(8, 125));
 
 				//オプション入るボタン
-				if (SimpleGUI::Button(U"オプション", Vec2(10, 170),120)) {
+				if (SimpleGUI::Button(U"オプション", Vec2(10, 170),125)) {
 					showoption = true;
 				}
 
 				//エディット入るボタン
-				if (SimpleGUI::Button(U"エディット", Vec2(135, 170),120)) {
+				if (SimpleGUI::Button(U"エディット", Vec2(140, 170),125)) {
 					//前のログを残しておき、エディット抜けたあと前の状態に復帰する
 					autosteplog = autostep;
 					autostep = false;
@@ -416,8 +465,8 @@ void Main()
 				fontsmall(U"live   dead").draw((width * ntimes) - 78, 21, Color(0, 0, 0));
 				for (auto i : step(9)) {
 					fontnormal(U"{}"_fmt(i)).draw((width * ntimes) - 97, 39 + (30 * i), Color(0, 0, 0));
-					SimpleGUI::CheckBox(livenum[i], U"", Vec2((width * ntimes) - 85, 37 + (30 * i)), 40);
-					SimpleGUI::CheckBox(deadnum[i], U"", Vec2((width * ntimes) - 45, 37 + (30 * i)), 40);
+					SimpleGUI::CheckBox(Rule.live[i], U"", Vec2((width * ntimes) - 85, 37 + (30 * i)), 40);
+					SimpleGUI::CheckBox(Rule.dead[i], U"", Vec2((width * ntimes) - 45, 37 + (30 * i)), 40);
 				}
 			}
 			else if (showoption && !toedit) {
@@ -435,7 +484,7 @@ void Main()
 					}
 				}
 
-				if (SimpleGUI::Slider(U"微調整",colorspeedadjust,0,0.05,Vec2(10,110),70,170));
+				SimpleGUI::Slider(U"微調整",colorspeedadjust,0,0.05,Vec2(10,110),70,170);
 				fontnormal(U"・青色の減衰速度").draw(10, 50, Color(0, 0, 0));
 
 				//表示倍率の変更を行うやつ
